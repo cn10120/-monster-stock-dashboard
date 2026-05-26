89248522f11f45e43dfc7443f27b0657b149ff80
cn10120 <curtissnagel@gmail.com>
2026-05-25 22:01:37 -0700

    Update HelpTip.tsx

--- HelpTip.tsx
+++ HelpTip.tsx
@@ -1,4 +1,4 @@
-import { helpLegend } from '../../lib/help';
+import { helpLegend } from './help';
 
 export default function HelpTip({ term }: { term: keyof typeof helpLegend }) {
   return <span className="ml-1 text-sky-300 help-tip" title={helpLegend[term]}>?</span>;
